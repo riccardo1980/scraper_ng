f85afac41606d7a7c716faafc5e0237e0f9a0c40
Riccardo Zanella <riccardo.zanella@gmail.com>
2019-04-13 14:26:41 +0200

    code reworking: all based on lists

--- common.py
+++ common.py
@@ -7,7 +7,7 @@ def get_page(url, unverified_ctx=False):
     if unverified_ctx:
         ssl._create_default_https_context = ssl._create_unverified_context
 
-    return urlopen(url).read()
+    return urlopen(url).read().decode('utf-8')
 
 def save_img(img_src, img_dest, unverified_ctx=False):
     if unverified_ctx:
--- shutterstock_scraper.py
+++ shutterstock_scraper.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from bs4 import BeautifulSoup
+import re
 from common import get_page, save_img
 
 def compose_page_url(search_string, page):
@@ -13,9 +14,28 @@ def compose_page_url(search_string, page):
     url += "&page="+str(page)
     return url
 
+def get_data_from_url(page_url_list):
+    total_page_url=len(page_url_list)
+    page_data_list = []
+    for idx, page_url in enumerate(page_url_list):
+
+        logging.info('Downloading page %d/%d %s', 
+                    idx+1, 
+                    total_page_url,
+                    page_url)
+        try:
+            data=get_page(page_url, 
+                          unverified_ctx=True)
+            page_data_list.append(data)
+
+        except Exception as e:
+            logging.warning(e)
+
+    return page_data_list
+
 def scrape_page(data):
     scraper = BeautifulSoup(data, "lxml")
-    container_list = scraper.find_all("div", {"class":"z_c_b"})
+    container_list = scraper.find_all("div", {"class":"z_e_b"})
     img_uris = []
     for container in container_list:
         imgs = container.find_all("img")
@@ -23,6 +43,49 @@ def scrape_page(data):
             img_uris.append(img.get("src"))
 
     return img_uris
+
+def scrape_pages(page_data_list):
+    total_page_data = len(page_data_list)
+    img_url_list = []
+    for idx, page_data in enumerate(page_data_list):
+
+        logging.info('Parsing page %d/%d', 
+                    idx+1, 
+                    total_page_data)
+        try:
+            imgs = scrape_page(page_data)
+            img_url_list.extend(imgs)
+
+        except Exception as e:
+            logging.warning(e)
+
+    return img_url_list
+
+def get_total_pages(data):
+    scraper = BeautifulSoup(data, "lxml")
+    end_string = scraper.find_all("div", {"class":"b_M_f"})[0].text
+    pattern = re.compile('[a-zAZ]*\s(\d+)$')
+    pages = int(pattern.findall(end_string)[0])
+    
+    return pages 
+
+def save_images(img_url_list, destination):
+    total_images=len(img_url_list)
+    for idx,img_src in enumerate(img_url_list):
+       url = "https://image.shutterstock.com/"
+       img_name = img_src.replace(url,'').replace('/','_')
+       img_dest = os.path.join(destination, 
+                               img_name)
+
+       logging.info('Downloading item %d/%d %s', 
+                    idx+1, 
+                    total_images,
+                    img_src)
+       try:
+           save_img(img_src, img_dest, unverified_ctx=True)
+
+       except Exception as e:
+           logging.warning(e)
 
 def imagescrape(search_terms, destination, pages):
     """
@@ -44,26 +107,25 @@ def imagescrape(search_terms, destination, pages):
     """
 
     search_string = '+'.join(search_terms)
-    try:
-        for page in range(1, pages+1):
+   
+    # get first page to have bounds
+    url = compose_page_url(search_string, 1)
+    data = get_page(url, unverified_ctx=True)
+    pages_tot = get_total_pages(data)
+    pages_max = min(pages, pages_tot) 
+    
+    # compose url list
+    page_url_list = []
+    for page in range(1, pages_max+1):
+        page_url_list.append(compose_page_url(search_string,
+                                              page))
+    
+    # get data from url
+    page_data_list = get_data_from_url(page_url_list)
+  
+    # get img list for url
+    img_url_list = scrape_pages(page_data_list)
+ 
+    # download img
+    save_images(img_url_list, destination)
 
-            logging.info('Getting page %d', page)
-            url = compose_page_url(search_string, page)
-            data = get_page(url, unverified_ctx=True)
-
-            logging.info('Parsing page %d', page)
-            img_uris = scrape_page(data)
-
-            for img_src in img_uris:
-                img_name = os.path.basename(img_src)
-                img_dest = os.path.join(destination, img_name)
-
-                logging.info('Downloading %s', img_src)
-                try:
-                    save_img(img_src, img_dest, unverified_ctx=True)
-
-                except Exception as e:
-                    logging.warning(e)
-
-    except Exception as e:
-        logging.warning(e)
